12bc95caab28a8770a370746428136f6f509a228
EnzoAGW <enzogonzaga@aluno.fapce.edu.br>
2024-03-30 17:54:06 -0300

    Criação do modal

--- App.js
+++ App.js
@@ -1,34 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import { Dimensions, Image, StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import { Dimensions, Image, StyleSheet, Text, TouchableOpacity, View, Modal } from 'react-native';
+import Slider from '@react-native-community/slider';
+import { ModalPassword } from './src/components/modal';
 
 
-
+var screen = Dimensions.get('window');
 
 export default function App() {
+  const [size, setSize] = useState(10)
+  const [passswordValue, setpassswordValue] = useState('')
+  const [showModal, setshowModal] = useState(false);
+
+  function generatePass() {
+    const caracteres = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-+<>?/";
+    let senha = "";
+    for (let i = 0; i < size; i++) {
+      const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
+      senha += caracteres[indiceAleatorio];
+    }
+    setpassswordValue(senha);
+    setshowModal(true);
+  }
   return (
     <View style={styles.container}>
       <Image
-      resizeMode = 'cover'
-      source={require("./assets/3567818.jpg")}
-      style={styles.img}
+        source={require("./src/assets/3567818.jpg")}
+        style={styles.img}
       />
-      <StatusBar style="auto" />
+      <Text style={styles.title}> {size} caracteres</Text>
+      <View style={styles.area}>
+        <Slider
+          style={{ height: 50 }}
+          minimumValue={6}
+          maximumValue={20}
+          thumbTintColor='#FF4F5A'
+          minimumTrackTintColor='#433633'
+          value={size}
+          onValueChange={(e) => {
+            setSize(e.toFixed(0))
+          }}
+        />
+      </View>
+      <TouchableOpacity style={styles.button} onPress={generatePass}>
+        <Text style={styles.textButton}>
+          Generate Passsword
+        </Text>
+      </TouchableOpacity>
+      <Modal visible={showModal} animationType='fade'>
+          <ModalPassword passWord={passswordValue} handleClose={()=>setshowModal(false)}/>
+      </Modal>
     </View>
   );
 }
-  var {screen} = Dimensions.get('window');
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#353535',
+    backgroundColor: '#fff',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    color: '#fff',
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#000000',
+  },
+  area: {
+    marginTop: 14,
+    marginBottom: 14,
+    width: '80%',
+    backgroundColor: '#fff',
+    padding: 8,
   },
   img: {
-    width: screen * 0.5
+    resizeMode: 'contain',
+    width: screen['width'] * 0.8,
+    height: screen['height'] * 0.3,
+    // height: 10,
+
+  },
+  button: {
+    backgroundColor: '#385A64',
+    borderRadius: 8,
+    width: '80%',
+    height: '6%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 18,
+    marginBottom: 18,
+  },
+  textButton: {
+    color: '#fff'
   },
 });
---
+++ src/components/modal/index.js
@@ -0,0 +1,80 @@
+import {View, Text, StyleSheet, Pressable, TouchableOpacity} from 'react-native';
+export function ModalPassword({ passWord, handleClose }){
+    return(
+        <View style={styles.container}>
+            <View style={styles.content}>
+                <Text style={styles.title}>Senha gerada</Text>
+                <Pressable style={styles.innerPassword}>
+                    <Text style={styles.text}>{passWord}</Text>
+                </Pressable>
+                <View style={styles.buttonArea} >
+                    <TouchableOpacity style={styles.button} onPress={handleClose}>
+                        <Text style={styles.buttonText}>Voltar</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.button,styles.buttonCopy]}>
+                        <Text style={styles.buttonCopyText}>Copiar senha</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: 'rgba(24,24,24,0.6)',
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    content:{
+        backgroundColor: '#fff',
+        width:"85%",
+        paddingTop:24,
+        paddingBottom:24,
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius:8,
+    },
+    title:{
+        fontSize:20,
+        fontWeight:"bold",
+        color:'#000',
+        marginBottom:24
+    },
+    text:{
+        fontSize:20,
+        fontWeight:"bold",
+        color:'#fff',
+        marginVertical:12,
+        textAlign:'center'
+
+    },
+    innerPassword:{
+        backgroundColor:'#0e0e0e',
+        width:"90%",
+        padding:5,
+        borderRadius:8
+    },
+    buttonArea:{
+        flexDirection:'row',
+        width:'90%',
+        marginTop:10,
+        alignItems:'center',
+        justifyContent:'space-between'
+    },
+    button:{
+        flex:1,
+        alignItems:'center',
+        marginVertical:14,
+        padding:8
+    },
+    buttonCopy:{
+        backgroundColor: '#385A64',
+        marginVertical:14,
+        borderRadius:8
+    },
+    buttonCopyText:{
+        color:'#fff',
+        fontWeight:'bold'
+    }
+}) 
